afce74db080c7b7e20d290e45f6dfb37e184ebac
lin640826 <lin640826@users.noreply.github.com>
2026-09-21 13:18:20 +0800

    Add Chuck level 9-point module

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='equipment-measurement-pwa-v2';
+const CACHE='equipment-measurement-pwa-v3';
 const ASSETS=['./','./index.html','./manifest.json','./html2canvas.min.js','./icon-192.png','./icon-512.png','./apple-touch-icon.png'];
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
